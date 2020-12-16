278fc00bfc371144693c5d4d8a04379bd164f16f
eliko101101 <eliko101101@gmail.com>
2020-12-16 17:22:02 +0200

    3

--- lab2.py
+++ lab2.py
@@ -2,16 +2,23 @@ class Weight:
     def __init__(self, m):
         self.m = m
 
+    def to_centner(self):
+        self.m=self.m/100
+        return self.m
+
     def __add__(self, n):
-        res = self.m + n
+        res = self.m/100 + n/100
         return res
 
     def __sub__(self, n):
         if n > self.m:
             raise ValueError
-        res = self.m - n
+        res = self.m/100 - n/100
         return res
 
     def __mul__(self, n):
-        res = self.m * n
+        res = self.m/100 * n/100
         return res
+
+print(Weight(100).to_centner())
+print(Weight(267).__add__(300))
--- test.py
+++ test.py
@@ -5,19 +5,19 @@ from lab2 import Weight
 class TestProgression(unittest.TestCase):
     test = Weight(2.56)
     def test_add__(self):
-        self.assertEqual(self.test.__add__(3), 5.5600000000000005)
-        self.assertEqual(self.test.__add__(2.44), 5)
+        self.assertEqual(self.test.__add__(3), 0.0556)
+        self.assertEqual(self.test.__add__(2.44), 0.05)
         self.assertTrue(self.test.__add__(487), isinstance(self.test.__add__(487), float))
 
     def test_sub__(self):
-        self.assertEqual(self.test.__sub__(2), 0.56)
-        self.assertEqual(self.test.__sub__(1), 1.56)
+        self.assertEqual(self.test.__sub__(2), 0.005600000000000001)
+        self.assertEqual(self.test.__sub__(1), 0.015600000000000001)
         self.assertRaises(ValueError, self.test.__sub__, 6)
 
 
     def test_mul__(self):
-        self.assertEqual(self.test.__mul__(2), 5.12)
-        self.assertEqual(self.test.__mul__(4), 10.24)
+        self.assertEqual(self.test.__mul__(2), 0.000512)
+        self.assertEqual(self.test.__mul__(4), 0.001024)
 
 
 
